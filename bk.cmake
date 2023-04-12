d7258b7edcceb5aca89626118622e82c498ebc7d
BenKoehler <ben.koehler@web.de>
2023-04-12 17:28:34 +0200

    change cmake; include source files directly in projects

--- bk.cmake
+++ bk.cmake
@@ -1,10 +1,17 @@
 set(BK_IMGUI_VERSION "1.89.1")
 
-add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/imgui EXCLUDE_FROM_ALL)
-ConfigureCompileOptions(imgui PUBLIC)
+set(imgui_srcs
+        ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/imgui/imgui.cpp
+        ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/imgui/imgui_demo.cpp
+        ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/imgui/imgui_draw.cpp
+        ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/imgui/imgui_tables.cpp
+        ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/imgui/imgui_widgets.cpp
+        )
+set(imgui_glfw_srcs ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/imgui/backends/imgui_impl_glfw.cpp)
+set(imgui_opengl3_srcs ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/imgui/backends/imgui_impl_opengl3.cpp)
+set(imgui_sdl_srcs ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/imgui/backends/imgui_impl_sdl.cpp)
 
 function(link_imgui target MOD)
-    target_link_libraries(${target} ${MOD} imgui)
     target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/imgui)
     target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/imgui/backends)
 endfunction()
